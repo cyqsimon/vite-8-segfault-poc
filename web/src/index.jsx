ebe031c80561f8e8810f2b6c9e3e8156ea49b614
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2026-07-21 04:20:53 +0800

    Remove all noise

--- web/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
-import registerSW from "./registerSW";
-
-registerSW();
-
-const root = createRoot(document.querySelector("#root"));
-root.render(<App />);
